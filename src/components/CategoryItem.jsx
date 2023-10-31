3a2f9d88e97e1860ea637bd481ab5ba14330482b
hairunisa0929 <hairunisa.learning@gmail.com>
2023-10-31 14:37:50 +0700

    add style on hover category

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,5 +1,9 @@
 function CategoryItem({ category }) {
-  return <div className="rounded-lg p-2 border-primary border-2">{category}</div>;
+  return (
+    <div className="rounded-lg p-2 border-primary border-2 hover:bg-primary hover:text-white cursor-pointer">
+      {category}
+    </div>
+  );
 }
 
 export default CategoryItem;
