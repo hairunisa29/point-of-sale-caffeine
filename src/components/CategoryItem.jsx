424399bdc145b7163acc2a39ca6976376393ac91
hairunisa29 <hairunisa2909@outlook.com>
2023-11-01 14:09:18 +0700

    change category item container background color

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,6 +1,6 @@
 function CategoryItem({ category }) {
   return (
-    <div className="rounded-lg p-2 border-primary border-2 hover:bg-primary hover:text-white cursor-pointer">
+    <div className="rounded-lg p-2 bg-white shadow-sm shadow-indigo-100 hover:bg-primary hover:text-white cursor-pointer">
       {category}
     </div>
   );
