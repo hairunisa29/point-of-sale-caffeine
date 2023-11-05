fdb420e97b2cb3a85070194e9551ae02d8df64b5
hairunisa29 <hairunisa2909@outlook.com>
2023-11-05 21:20:06 +0700

    remove zero decimal

--- src/utils/formatter.js
+++ src/utils/formatter.js
@@ -2,5 +2,5 @@ export const formatCurrency = (price) => {
     return new Intl.NumberFormat("id-ID", {
       style: "currency",
       currency: "IDR",
-    }).format(price);
+    }).format(price).replace(/(\.|,)00$/g, '');
   };
