98db220d2f375981a1d3482bedd4ffa9166ecb64
hairunisa29 <hairunisa2909@outlook.com>
2023-11-08 14:55:26 +0700

    add table transaction

--- src/pages/TransactionHistoryPage.jsx
+++ src/pages/TransactionHistoryPage.jsx
@@ -1,12 +1,46 @@
+import { useMemo } from "react";
 import axios from "axios";
 import moment from "moment";
 import Swal from "sweetalert2";
 import useSWR from "swr";
 import { HiEye } from "react-icons/hi";
-import { formatCurrency } from "../utils/formatter";
 import { SyncLoader } from "react-spinners";
+import { formatCurrency } from "../utils/formatter";
+import Table from "../components/Table";
 
 function TransactionHistoryPage() {
+  const columns = useMemo(
+    () => [
+      {
+        Header: "Transaction Date",
+        accessor: "createdAt",
+        Cell: ({ value }) => moment(value).format("l"),
+      },
+      {
+        Header: "Transaction ID",
+        accessor: "id",
+        disableSortBy: true,
+      },
+      {
+        Header: "Total Price",
+        accessor: "totalPrice",
+        Cell: ({ value }) => formatCurrency(value),
+      },
+      {
+        Header: "Action",
+        disableSortBy: true,
+        Cell: () => {
+          return (
+            <button className="rounded-lg bg-primary hover:bg-red-700 text-white w-fit p-2">
+              <HiEye />
+            </button>
+          );
+        },
+      },
+    ],
+    []
+  );
+
   const fetchData = (url) => axios.get(url).then((response) => response.data);
 
   const { data, isLoading } = useSWR(
@@ -29,30 +63,9 @@ function TransactionHistoryPage() {
     <section className="p-8">
       <h1 className="text-2xl font-bold mb-4">Transaction History</h1>
 
-      <div className="flex flex-col gap-4 p-4 border-[1px] rounded-lg">
-        <div className="grid grid-cols-4 font-bold">
-          <span>Transaction Date</span>
-          <span>Transaction ID</span>
-          <span>Total Price</span>
-          <span>Action</span>
-        </div>
-
-        <hr />
-
+      <div className="flex flex-col gap-4 p-4 border-[1px] rounded-lg bg-white shadow-md">
         {!isLoading ? (
-          data?.map((item) => (
-            <div key={item.id} className="grid grid-cols-4 text-sm">
-              <span>{moment(item.createdAt).format("l")}</span>
-              <span>{item.id}</span>
-              <span>{formatCurrency(item.totalPrice)}</span>
-              <button
-                className="rounded-lg bg-primary hover:bg-red-700 text-white w-fit p-2"
-                // onClick={handleFinishPayment}
-              >
-                <HiEye />
-              </button>
-            </div>
-          ))
+          <Table columns={columns} data={data} />
         ) : (
           <SyncLoader color="#e55644" />
         )}
