fb882fcf30a5d83d6f0e0a1206bc6fc2390b2e9e
hairunisa29 <hairunisa2909@outlook.com>
2023-11-10 16:26:55 +0700

    enhance style order detail item

--- src/components/OrderDetailItem.jsx
+++ src/components/OrderDetailItem.jsx
@@ -2,17 +2,17 @@ import { formatCurrency } from "../utils/formatter";
 
 function OrderDetailItem({ image, name, price, qty }) {
   return (
-    <div className="grid grid-cols-4 gap-4">
+    <div className="grid grid-cols-4 gap-4 bg-white rounded-lg p-4">
       <div className="col-span-2 flex gap-4">
         <div className="rounded-lg w-20 h-20">
           <img
             src={image}
             alt={name}
-            className="rounded-lg w-full h-full object-cover"
+            className="rounded-lg w-full h-full object-cover shadow-md"
           />
         </div>
 
-        <div>
+        <div className="self-center">
           <h3 className="font-bold">{name}</h3>
           <span className="font-bold text-primary">
             {formatCurrency(price)}
@@ -20,9 +20,9 @@ function OrderDetailItem({ image, name, price, qty }) {
         </div>
       </div>
 
-      <span>{qty}</span>
+      <span className="self-center">{qty}</span>
 
-      <span className="font-bold text-primary">
+      <span className="font-bold text-primary self-center">
         {formatCurrency(price * qty)}
       </span>
     </div>
