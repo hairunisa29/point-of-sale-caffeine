7d4f1dff8143d32ea3416beead5362898c64d314
hairunisa0929 <hairunisa.learning@gmail.com>
2023-11-14 10:33:17 +0700

    add handle modal and fix

--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -1,13 +1,16 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
+import axios from "axios";
 import { SyncLoader } from "react-spinners";
 import { BiEditAlt } from "react-icons/bi";
 import { RiDeleteBin5Line } from "react-icons/ri";
 import useSWR from "swr";
+import Modal from "../components/Modal";
 import Table from "../components/Table";
-import { formatCurrency } from "../utils/formatter";
 import { PopUpAlert } from "../utils/alert";
+import { formatCurrency } from "../utils/formatter";
 
 function MenuPage() {
+  const [showModal, setShowModal] = useState(false);
   const columns = useMemo(
     () => [
       {
@@ -15,13 +18,13 @@ function MenuPage() {
         Cell: (props) => props.cell.row.index + 1,
       },
       {
+        Header: "Product Name",
+        accessor: "name",
+      },
+      {
         Header: "Product ID",
         accessor: "id",
         disableSortBy: true,
-      },
-      {
-        Header: "Product Name",
-        accessor: "name",
       },
       {
         Header: "Category",
@@ -41,10 +44,10 @@ function MenuPage() {
         disableSortBy: true,
         Cell: (props) => {
           return (
-            <div className="flex">
+            <div className="flex gap-4">
               <button
                 className="rounded-lg bg-yellow-500 hover:bg-yellow-700 text-white w-fit p-2"
-                // onClick={() => navigate(`/history/${props.row.values.id}`)}
+                onClick={handleModalEdit(props.row.values.id)}
               >
                 <BiEditAlt />
               </button>
@@ -77,16 +80,29 @@ function MenuPage() {
     }
   );
 
+  const handleModalAdd = () => {
+    setShowModal(true);
+  };
+
+  const handleModalEdit = (id) => {};
+
+  const handleDelete = () => {};
+
   return (
     <section className="flex flex-col p-8">
       <h1 className="text-2xl font-bold mb-4">Menu</h1>
 
+      {showModal && <Modal />}
+
+      <button
+        className="ml-auto mb-4 rounded-lg bg-primary hover:bg-blue-700 text-white text-sm font-bold w-fit p-2"
+        onClick={handleModalAdd}
+      >
+        Add New Menu
+      </button>
+
       {!isLoading ? (
         <div className="flex flex-col gap-4 border-[1px] rounded-lg bg-white shadow-md">
-          <button className="justify-end rounded-lg bg-primary hover:bg-blue-700 text-white text-sm font-bold w-full p-2">
-            Add New Menu
-          </button>
-
           <Table columns={columns} data={data} />
         </div>
       ) : (
