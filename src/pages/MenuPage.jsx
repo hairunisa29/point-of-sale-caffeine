3b396115cc2c58a3602a9c55d945937d8e98259b
hairunisa0929 <hairunisa.learning@gmail.com>
2023-11-14 09:46:39 +0700

    finishing page menu

--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -1,6 +1,9 @@
 import { useMemo } from "react";
 import Table from "../components/Table";
 import { formatCurrency } from "../utils/formatter";
+import { PopUpAlert } from "../utils/alert";
+import useSWR from "swr";
+import { SyncLoader } from "react-spinners";
 
 function MenuPage() {
   const columns = useMemo(
@@ -25,6 +28,7 @@ function MenuPage() {
       {
         Header: "Price",
         accessor: "price",
+        Cell: ({ value }) => formatCurrency(value),
       },
       {
         Header: "Stock",
@@ -33,7 +37,34 @@ function MenuPage() {
     ],
     []
   );
-  return <div></div>;
+
+  const fetchData = (url) => axios.get(url).then((response) => response.data);
+
+  const { data, isLoading } = useSWR(
+    "http://localhost:3000/products",
+    fetchData,
+    {
+      onError: (error) => {
+        if (error) {
+          PopUpAlert("Error", error?.message, "error");
+        }
+      },
+    }
+  );
+
+  return (
+    <section className="flex flex-col p-8">
+      <h1 className="text-2xl font-bold mb-4">Menu</h1>
+
+      {!isLoading ? (
+        <div className="flex flex-col gap-4 border-[1px] rounded-lg bg-white shadow-md">
+          <Table columns={columns} data={data} />
+        </div>
+      ) : (
+        <SyncLoader color="#2457ca" />
+      )}
+    </section>
+  );
 }
 
 export default MenuPage;
