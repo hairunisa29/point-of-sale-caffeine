3f69d0d6d1461933f56b87d9f9257eb8b82c2004
hairunisa29 <hairunisa2909@outlook.com>
2023-11-15 15:40:22 +0700

    fix error form add product

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,10 +1,4 @@
-import {
-  Button,
-  Card,
-  CardHeader,
-  CardBody,
-  Dialog,
-} from "@material-tailwind/react";
+import { Card, CardBody, Dialog } from "@material-tailwind/react";
 
 function Modal({
   title,
@@ -24,60 +18,77 @@ function Modal({
     >
       <Card className="mx-auto w-full max-w-[24rem]">
         <CardBody>
-          <h1>{title}</h1>
+          <h1 className="mb-4 text-2xl font-bold">{title}</h1>
 
-          <form onSubmit={handleSubmit(onSubmitModal)}>
+          <form
+            className="flex flex-col gap-4 text-sm"
+            onSubmit={handleSubmit(onSubmitModal)}
+          >
             <div>
               <label htmlFor="productName">Product Name</label>
               <input
                 id="productName"
-                name="productName"
                 placeholder="Product Name"
                 className="w-full rounded-md border-[1px] border-gray-200 p-3 shadow-sm sm:text-sm"
                 {...register("productName")}
               />
-              <p className="error">{errors.name?.message}</p>
+              <p className="error">{errors.productName?.message}</p>
             </div>
 
             <div>
               <label htmlFor="category">Category</label>
               <select
                 id="category"
-                name="category"
                 placeholder="Category"
                 className="w-full rounded-md border-[1px] border-gray-200 p-3 shadow-sm sm:text-sm"
                 {...register("category")}
               >
-                <option></option>
-                <option></option>
-                <option></option>
+                <option value="Coffee">Coffee</option>
+                <option value="Non Coffee">Non Coffee</option>
+                <option value="Pastry">Pastry</option>
               </select>
-              <p className="error">{errors.name?.message}</p>
+              <p className="error">{errors.category?.message}</p>
             </div>
 
             <div>
               <label htmlFor="price">Price</label>
               <input
                 id="price"
-                name="price"
                 placeholder="Price"
                 className="w-full rounded-md border-[1px] border-gray-200 p-3 shadow-sm sm:text-sm"
                 {...register("price")}
               />
-              <p className="error">{errors.name?.message}</p>
+              <p className="error">{errors.price?.message}</p>
             </div>
 
             <div>
               <label htmlFor="stock">Stock</label>
               <input
                 id="stock"
-                name="stock"
                 placeholder="Stock"
                 className="w-full rounded-md border-[1px] border-gray-200 p-3 shadow-sm sm:text-sm"
                 {...register("stock")}
               />
-              <p className="error">{errors.name?.message}</p>
+              <p className="error">{errors.stock?.message}</p>
             </div>
+
+            <div>
+              <label htmlFor="image">Upload Image</label>
+              <input
+                id="image"
+                type="file"
+                className="w-full rounded-md border-[1px] border-gray-200 p-3 shadow-sm sm:text-sm"
+                {...register("image")}
+              />
+              <p className="error">{errors.image?.message}</p>
+            </div>
+
+            <button
+              className={`w-full p-2 rounded-lg bg-primary hover:bg-blue-700 text-white text-sm font-bold`}
+              // onClick={handleFinishPayment}
+            >
+              Submit
+            </button>
           </form>
         </CardBody>
       </Card>
