3d588fe485fb98cc18ae6f664c25cc3305d9e9e6
hairunisa29 <hairunisa2909@outlook.com>
2023-11-16 13:48:27 +0700

    uninstall package material tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { ThemeProvider } from "@material-tailwind/react";
 import store from "./store/store.js";
 import App from "./App.jsx";
 import "./index.css";
@@ -10,11 +9,9 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ThemeProvider>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </ThemeProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
-const withMT = require("@material-tailwind/react/utils/withMT");
-
 /** @type {import('tailwindcss').Config} */
-module.exports = withMT({
+export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
@@ -14,4 +12,4 @@ module.exports = withMT({
     },
   },
   plugins: [],
-});
+};
